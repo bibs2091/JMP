af8249d49f1c0cf91ec0dc8a8a99cfcd560c1dd6
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-03-28 15:26:07 +0100

    adding InfoSite model

---
+++ models/InfoSite.js
@@ -0,0 +1,50 @@
+const Sequelize = require("sequelize");
+const db = require("../config/database");
+
+const InfoSite = db.define('InfoSite', {
+    nom: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    logo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        isEmail: true
+    },
+    adresse: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    inscription: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    },
+    terms: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lien_fcb: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lien_insta: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lien_twitter: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+db.sync({ forced: true });
+
+module.exports = InfoSite;
