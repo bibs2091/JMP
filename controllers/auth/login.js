2a303423499bae58a6e4148919bd4594eef433d5
Mou97 <m.taguiouine@esi-sba.dz>
2019-04-03 23:22:12 +0100

    Delete the controllers for auth

--- controllers/auth/login.js
+++
@@ -1,9 +0,0 @@
-const passport = require("passport");
-
-module.exports = (req, res, next) => {
-	passport.authenticate("local", (err, user, info) => {
-		req.login(user, err => {
-			return res.send("authenticated");
-		});
-	})(req, res, next);
-};
