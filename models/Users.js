6fd86578718e9a3bbd71856b7bbceaf3c7b408bf
Mou97 <m.taguiouine@esi-sba.dz>
2019-04-11 14:01:35 +0100

    add 'validated'  in the User model

--- models/Users.js
+++ models/Users.js
@@ -1,21 +1,26 @@
 const Sequelize = require("sequelize");
 const db = require("../config/database");
 
-const Users = db.define('Users', {
-    email: {
-        type: Sequelize.STRING,
-        notNull: true,
-        isEmail: true
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    groudId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: 2
-    }
+const Users = db.define("Users", {
+	email: {
+		type: Sequelize.STRING,
+		notNull: true,
+		isEmail: true,
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	groudId: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+		defaultValue: 2,
+	},
+	validated: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false,
+	},
 });
 
 db.sync({ forced: true });
