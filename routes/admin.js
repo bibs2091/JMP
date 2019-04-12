2c469e951c640c3b636bcee0ebef6e5b0e7e14e1
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-04-12 23:02:41 +0100

    delete category backend

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,7 @@ const categoriesController = require("../controllers/admin/categories");
 const addCategoryController = require("../controllers/admin/addCategory");
 const coursesController = require("../controllers/admin/courses");
 const getCategoryController = require("../controllers/admin/getCategory");
+const deleteCategoryController = require("../controllers/admin/deleteCategory");
 
 //handling requests 
 router.get("/", (req, res) => {
@@ -17,5 +18,5 @@ router.get("/categories", categoriesController);
 router.post("/addcategory", addCategoryController);
 router.get("/courses", coursesController);
 router.get("/category/:id", getCategoryController);
-
+router.delete("/category/delete/:id", deleteCategoryController);
 module.exports = router;
