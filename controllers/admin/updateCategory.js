505cf29ac82a4717677fff44da185462edf5fa7c
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-04-12 23:56:02 +0100

    fixing update category bug

--- controllers/admin/updateCategory.js
+++ controllers/admin/updateCategory.js
@@ -2,24 +2,32 @@ const Categories = require('../../models/Categories');
 const fs = require('fs');
 
 module.exports = (req, res) => {
-    const { image } = req.files;
-    //delete old pic
-    fs.unlink(__dirname + '/../../public/categories/' + req.params.id + ".jpg", function (error) {
-        image.mv(__dirname + "/../../public/categories/" + req.params.id + ".jpg",
-            (err) => {
-                if (err) {
-                    console.log(err)
-                }
-                Categories.update(
-                    req.body,
-                    { where: { id: req.params.id } }
-                ).then(() => {
-                    res.redirect("/admin/categories");
+    if (req.files) {
+        const { image } = req.files;
+        //delete old pic
+        fs.unlink(__dirname + '/../../public/categories/' + req.params.id + ".jpg", function (error) {
+            image.mv(__dirname + "/../../public/categories/" + req.params.id + ".jpg",
+                (err) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    Categories.update(
+                        req.body,
+                        { where: { id: req.params.id } }
+                    ).then(() => {
+                        res.redirect("/admin/categories");
+                    });
                 });
-            });
-    });
-    //add the new pic
+        });
+    } else {
+        Categories.update(
+            req.body,
+            { where: { id: req.params.id } }
+        ).then(() => {
+            res.redirect("/admin/categories");
+        });
+    }
 
-    //update category infos
+
 
 };
