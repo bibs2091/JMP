49fce9d3051beff5d03c336866f8194fb6186f7c
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-04-12 19:19:25 +0100

    adding course details route and controller

--- routes/home.js
+++ routes/home.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 //require controllers 
 const homeController = require("../controllers/home");
-
+const courseDetailsController = require("../controllers/courseDetails");
 
 router.get("/", homeController);
 
 router.get("/home", (req, res) => {
 	res.redirect("/");
 });
+
+router.get("/course/:id", courseDetailsController);
 
 module.exports = router;
