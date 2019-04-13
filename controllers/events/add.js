f43893ce2a15d900c4aa11d4672476051ddeaf02
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-04-13 03:00:22 +0200

    emproved the add-event functionality

--- controllers/events/add.js
+++ controllers/events/add.js
@@ -1,9 +1,14 @@
 const Event = require('../../models/Event');
 module.exports = async(req, res) => {
 	// getting event infos
-	const creatorId = req.user.groupId;
+	const creatorId = req.user.Id;
     const {name,date,place,description,cover,nbPlace,planning,validated} = req.body;
     // creating the event proposition
     const newevent = await Event.create({name,date,place,description,cover,nbPlace,planning,creatorId,validated});
-    res.send(newevent);
+    if (req.user.groupId === 0){
+    	res.redirect('/admin/event');
+
+    }else if(req.user.groupId === 1){
+		res.redirect('/coach/event');    	
+    }
 }
