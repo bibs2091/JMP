77e45f4f95485a335cb76a7c4ba5394a4ea48494
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-04-13 02:50:39 +0200

    add event creation functionality

--- controllers/events/add.js
+++ controllers/events/add.js
@@ -1,3 +1,9 @@
-module.exports = (req, res) => {
-    res.send("add event");
+const Event = require('../../models/Event');
+module.exports = async(req, res) => {
+	// getting event infos
+	const creatorId = req.user.groupId;
+    const {name,date,place,description,cover,nbPlace,planning,validated} = req.body;
+    // creating the event proposition
+    const newevent = await Event.create({name,date,place,description,cover,nbPlace,planning,creatorId,validated});
+    res.send(newevent);
 }
