02e8de42ed82075ce393ef6655d5d70d712183ca
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-04-13 04:26:26 +0200

    added delete event functionality

--- controllers/events/add.js
+++ controllers/events/add.js
@@ -1,19 +1,32 @@
 const Event = require('../../models/Event');
 module.exports = async(req, res) => {
 	try{
+		const user = {groupId :1}
+		console.log(req.files);
 			// getting event infos
-		const creatorId = req.user.Id;
-	    const {name,date,place,description,cover,nbPlace,planning,validated,tags} = req.body;
+		const creatorId = 1;
+		const {cover,planning} = req.files; 
+	    const {name,date,place,description,nbPlace,validated,tags} = req.body;
 	    // creating the event proposition
-	    const newevent = await Event.create({name,date,place,description,cover,nbPlace,planning,creatorId,validated,tags});
-	    if (req.user.groupId === 0){
+	    const newevent = await Event.create({name,date,place,description,nbPlace,creatorId,validated,tags});
+	    Event.update(
+        {
+        	cover : '/img/events/covers'+newevent.id+".jpg",
+        	planning : '/img/events/plannings'+newevent.id+".jpg"
+    	},
+        {where : {id :newevent.id}}
+        )
+		await cover.mv(__dirname + '/../../public/img/events/covers/'+newevent.id+".jpg");
+	    await planning.mv(__dirname + '/../../public/img/events/plannings/'+newevent.id+".jpg");
+	    if (user.groupId === 0){
 	    	res.redirect('../admin/event');
 
-	    }else if(req.user.groupId === 1){
+	    }else if(user.groupId === 1){
 			res.redirect('/coach/event');    	
 	    }
 	}catch(err){
-		req.redirect('/errors');
+		console.log(err);
+		res.redirect('/errors');
 	}
 	
 }
--- routes/events.js
+++ routes/events.js
@@ -4,9 +4,10 @@ const router = express.Router();
 //require controllers
 const addController = require("../controllers/events/add");
 const validatingController = require("../controllers/events/validating");
+const deleteController = require("../controllers/events/delete");
 //handling requests 
 router.post("/validating/:id",validatingController);
-
+router.post("/delete/:id",deleteController);
 router.post("/add", addController);
 router.get("/add", (req, res) => {
     res.render("events.add");
