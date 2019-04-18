96168b62c2682f8c1d7444897aff92c1ab930219
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-04-18 17:10:32 +0100

    add course and save the cover image

--- controllers/courses/add.js
+++ controllers/courses/add.js
@@ -1,4 +1,15 @@
-module.exports = (req, res) => {
-    console.log(req.body.courseJSON);
-    res.redirect("/courses/add")
+const Courses = require("../../models/Courses");
+const Chapters = require("../../models/Chapters");
+const Lectures = require("../../models/Lectures");
+
+module.exports = async (req, res) => {
+    let courseDATA = req.body;
+    delete courseDATA.courseJSON;
+    //create the course
+    let course = await Courses.create(courseDATA);
+    let courseId = course.dataValues.id;
+    //save the cover image
+    const { cover } = req.files;
+    await cover.mv(__dirname + "/../../public/img/courses/" + courseId + ".jpg");
+    res.redirect("/courses/add");
 }
