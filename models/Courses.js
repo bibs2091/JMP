a71cea28302e9883118f04649424ace91fac63a8
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-04-18 16:39:58 +0100

    adding Courses modal

---
+++ models/Courses.js
@@ -0,0 +1,35 @@
+const Sequelize = require("sequelize");
+const db = require("../config/database");
+
+const Courses = db.define("Courses", {
+    category: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    cover: {
+        type: Sequelize.STRING
+    },
+    level: {
+        type: Sequelize.STRING
+    },
+    description: {
+        type: Sequelize.STRING
+    },
+    author: {
+        type: Sequelize.STRING
+    },
+    rating: {
+        type: Sequelize.INTEGER
+    },
+    tags: {
+        type: Sequelize.STRING
+    }
+});
+
+db.sync({ forced: true });
+
+module.exports = Courses;
