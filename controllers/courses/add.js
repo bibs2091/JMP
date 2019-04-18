03169f98e9a166b87e348c197cfa77268d3b16e1
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-04-18 17:31:09 +0100

    finishing add course feature

--- controllers/courses/add.js
+++ controllers/courses/add.js
@@ -4,6 +4,7 @@ const Lectures = require("../../models/Lectures");
 
 module.exports = async (req, res) => {
     let courseDATA = req.body;
+    let courseJSON = JSON.parse(req.body.courseJSON);
     delete courseDATA.courseJSON;
     //create the course
     let course = await Courses.create(courseDATA);
@@ -11,5 +12,22 @@ module.exports = async (req, res) => {
     //save the cover image
     const { cover } = req.files;
     await cover.mv(__dirname + "/../../public/img/courses/" + courseId + ".jpg");
-    res.redirect("/courses/add");
+    //adding chapter
+    var currentChapter = {};
+    for (let i = 0; i < courseJSON.length; i++) {
+        currentChapter = courseJSON[i];
+        let chapter = await Chapters.create({
+            title: currentChapter.title,
+            formation: courseId
+        });
+        let chapterId = chapter.dataValues.id;
+        var lectures = currentChapter.lectures;
+        var currentLecture = {};
+        for (let j = 0; j < lectures.length; j++) {
+            currentLecture = lectures[j];
+            currentLecture.chapter = chapterId;
+            let lecture = await Lectures.create(currentLecture);
+        }
+    }
+    res.redirect("/courses/" + courseId);
 }
