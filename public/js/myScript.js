58c699352ddfd1a66a2f272ba8495a220d6348c5
bibs2091 <m.naas@esi-sba.dz>
2019-04-20 00:32:41 +0100

    adjust add course page + add tags

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -11,4 +11,28 @@ function activateLoginButton() {
      $(this).next().slideToggle("slow");
  })
 
-
+             //------ tags ------
+ $(document).ready( function() {
+    $("#tag-typer").keypress( function(event) {
+      var key = event.which;
+      if (key == 13 || key == 44){
+       event.preventDefault();
+       var tag = $(this).val();
+        if(tag.length > 0){
+          $("<span class='tag' style='display:none'><span class='close'>&times;</span>"+tag+" </span>").insertBefore(this).fadeIn(100);
+          $(this).val("");
+        }
+      }
+    });
+    
+    $("#tags").on("click", ".close", function() {
+      $(this).parent("span").fadeOut(100);
+    });
+    
+    $(".colors li").click(function() {
+      var c = $(this).css("background-color");
+      $(".tag").css("background-color",c);
+      $("#title").css("color",c);
+    });
+    
+  });
