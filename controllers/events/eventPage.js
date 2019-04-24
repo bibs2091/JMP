2d414bf3a201053536293361c9299adb8be9bcbc
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-04-24 02:29:00 +0100

    redirect to error 404 if event doesn't exist

--- controllers/events/eventPage.js
+++ controllers/events/eventPage.js
@@ -1,8 +1,11 @@
 const Event = require('../../models/Event');
-module.exports = async(req, res) => {
+module.exports = async (req, res) => {
 	const event = await Event.findByPk(req.params.id);
-	event.dataValues.date.getFullYear()  + '-' + ('0'+(event.dataValues.date.getMonth()+1)).slice(-2) + '-' + ('0'+event.dataValues.date.getDate()).slice(-2);
-	res.render('events.event',{
+	if (!event) {
+		return res.render("404");
+	}
+	event.dataValues.date.getFullYear() + '-' + ('0' + (event.dataValues.date.getMonth() + 1)).slice(-2) + '-' + ('0' + event.dataValues.date.getDate()).slice(-2);
+	res.render('events.event', {
 		event
 	});
 }
