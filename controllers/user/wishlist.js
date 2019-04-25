65614a475999a8c81adedde9639c88f6e9535b82
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-04-25 13:24:46 +0100

    showing the wishlist to the user

--- controllers/user/wishlist.js
+++ controllers/user/wishlist.js
@@ -1,6 +1,17 @@
-module.exports = (req, res) => {
+const WishLists = require("../../models/WishLists");
+const Courses = require("../../models/Courses");
+
+module.exports = async (req, res) => {
+    const userId = req.user.id;
+    let wishlist = await WishLists.findAll({ where: { userId } });
+    var courses = [];
+    for (let i = 0; i < wishlist.length; i++) {
+        let course = await Courses.findByPk(wishlist[i].dataValues.id);
+        courses.push(course);
+    }
     res.render("user.wishlist", {
         pageName: "My Wishlist",
-        pageTitle: "My Wishlist - JMP"
+        pageTitle: "My Wishlist - JMP",
+        courses
     });
 }
