f6cd988cb8e684a5bcc9abe24ac57afcbbd7d22d
bibs2091 <m.naas@esi-sba.dz>
2019-04-26 01:17:38 +0100

    share course small popup

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -59,3 +59,14 @@ $("#delete_sponsor").click(function(){
   countSP= countSP -1;
 }});
 });
+//----------- share -------------
+$( document ).ready(function() {
+	//custom button for homepage
+     $( ".share-btn" ).click(function(e) {
+     	 $('.networks-5').not($(this).next( ".networks-5" )).each(function(){
+         	$(this).removeClass("active");
+    	 });
+     
+            $(this).next( ".networks-5" ).toggleClass( "active" );
+    });   
+});
