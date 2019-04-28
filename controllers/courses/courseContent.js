93db57e33acdf00910b56e481f079f37e8f43523
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-04-28 02:55:43 +0100

    generate youtube embed link

--- controllers/courses/courseContent.js
+++ controllers/courses/courseContent.js
@@ -4,57 +4,64 @@ const Lectures = require("../../models/Lectures");
 const Progress = require("../../models/Progress");
 
 module.exports = async (req, res) => {
-    var courseId = req.params.course;
-    var progress = await Progress.findOne({
-        where: {
-            userId: req.user.id,
-            courseId
-        }
-    });
-    var content = await Lectures.findByPk(req.params.lecture);
-    if (!content) {
-        return res.render("404");
-    }
-    if (req.params.lecture == progress.lastLecture + 1) {
-        await Progress.update({ lastLecture: req.params.lecture }, {
+    try {
+        var courseId = req.params.course;
+        var progress = await Progress.findOne({
             where: {
                 userId: req.user.id,
                 courseId
             }
         });
-    } else if (req.params.lecture > progress.lastLecture) {
-        return res.render("404");
-    }
-    var newProgress = await Progress.findOne({
-        where: {
-            userId: req.user.id,
-            courseId
+        var content = await Lectures.findByPk(req.params.lecture);
+        if (!content) {
+            return res.render("404");
         }
-    });
-    var course = await Courses.findByPk(courseId);
-    var chaptersList = [];
-    var chaps = await Chapters.findAll({ where: { formation: courseId } });
-    for (let i = 0; i < chaps.length; i++) {
-        var currentChap = {};
-        currentChap.title = chaps[i].title;
-        var lects = await Lectures.findAll({ where: { chapter: chaps[i].id } });
-        var currentLects = [];
-        for (let j = 0; j < lects.length; j++) {
-            let obj = {};
-            obj.id = lects[j].id;
-            obj.title = lects[j].title;
-            currentLects.push(obj);
+        if (req.params.lecture == progress.lastLecture + 1) {
+            await Progress.update({ lastLecture: req.params.lecture }, {
+                where: {
+                    userId: req.user.id,
+                    courseId
+                }
+            });
+        } else if (req.params.lecture > progress.lastLecture) {
+            return res.render("404");
+        }
+        var newProgress = await Progress.findOne({
+            where: {
+                userId: req.user.id,
+                courseId
+            }
+        });
+        var course = await Courses.findByPk(courseId);
+        var chaptersList = [];
+        var chaps = await Chapters.findAll({ where: { formation: courseId } });
+        for (let i = 0; i < chaps.length; i++) {
+            var currentChap = {};
+            currentChap.title = chaps[i].title;
+            var lects = await Lectures.findAll({ where: { chapter: chaps[i].id } });
+            var currentLects = [];
+            for (let j = 0; j < lects.length; j++) {
+                let obj = {};
+                obj.id = lects[j].id;
+                obj.title = lects[j].title;
+                currentLects.push(obj);
+            };
+            currentChap.lectures = currentLects;
+            chaptersList.push(currentChap);
         };
-        currentChap.lectures = currentLects;
-        chaptersList.push(currentChap);
-    };
 
 
-    res.render("courses.course", {
-        pageTitle: content.title,
-        chaptersList,
-        content,
-        newProgress,
-        courseId
-    });
+        res.render("courses.course", {
+            pageTitle: content.title,
+            chaptersList,
+            content,
+            newProgress,
+            courseId
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.redirect("/error");
+    }
+
 }
