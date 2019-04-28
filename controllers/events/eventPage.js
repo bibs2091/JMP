a7cbd22ac4cfbc728b6092e65a7553c47b8ebb82
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-04-28 05:28:52 +0200

    adding sponsors to the eventPage view

--- controllers/events/eventPage.js
+++ controllers/events/eventPage.js
@@ -1,13 +1,21 @@
 const Event = require('../../models/Event');
+const Sponsor = require('../../models/Sponsors');
 module.exports = async (req, res) => {
 	try{
+		const sponsors = await Sponsor.findAll(
+			{where :
+				{
+					eventId : req.params.id
+				}})
+		console.log(sponsors);
 		const event = await Event.findByPk(req.params.id);
 		if (!event) {
 			return res.render("404");
 		}
 		event.dataValues.date.getFullYear() + '-' + ('0' + (event.dataValues.date.getMonth() + 1)).slice(-2) + '-' + ('0' + event.dataValues.date.getDate()).slice(-2);
 		res.render('events.event', {
-			event
+			event,
+			sponsors
 		});
 	} catch (err){
 		console.log(err);
