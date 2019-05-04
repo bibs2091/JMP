75f1784190acbc162b36b85221590444833d5865
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-05-04 19:09:12 +0100

    setup coach courses page

--- controllers/coach/mycourses.js
+++ controllers/coach/mycourses.js
@@ -1,14 +1,16 @@
 const UsersInfo = require("../../models/UsersInfo");
+const Courses = require("../../models/Courses");
 
 module.exports = async (req, res) => {
+    //get current user info
     var currentUser = req.user;
     var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
     delete currentUser.password;
     currentUser.info = userInfo.dataValues;
 
     res.render("coach.home", {
-        pageName: "Home",
-        pageTitle: currentUser.info.username + " - Home",
+        pageName: "My Courses",
+        pageTitle: currentUser.info.username + " - My Courses",
         currentUser
     });
 };
