b9ce6af86751ac33ac0c958d44d30c4b8d62c21d
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-05-04 04:28:33 +0100

    adding techReports model

---
+++ models/techReports.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const db = require("../config/database");
+
+const techReports = db.define("techReports", {
+    title: {
+        type: Sequelize.STRING
+    },
+    description: {
+        type: Sequelize.INTEGER
+    }
+});
+
+db.sync({ forced: true });
+
+module.exports = techReports;
