0b2fd0b68c381815434c22e43981bcde7e0073f7
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-05-04 19:01:53 +0100

    setup coach home page

--- controllers/coach/home.js
+++ controllers/coach/home.js
@@ -1,6 +1,14 @@
-module.exports = (req, res) => {
+const UsersInfo = require("../../models/UsersInfo");
+
+module.exports = async (req, res) => {
+    var currentUser = req.user;
+    var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
+    delete currentUser.password;
+    currentUser.info = userInfo.dataValues;
+
     res.render("coach.home", {
         pageName: "Home",
-        pageTitle: "username - Home"
+        pageTitle: "username - Home",
+        currentUser
     });
 };
