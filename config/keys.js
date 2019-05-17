278048550a58c57e3987be98fac4e9fe22d60cf2
Mou97 <m.taguiouine@esi-sba.dz>
2019-05-17 16:28:27 +0100

    set up vapid keys for web-push

--- config/keys.js
+++ config/keys.js
@@ -3,4 +3,8 @@ module.exports = {
 		clientId: "1950c5657151a54e493d",
 		clientSecret: "e2221416d9b3f3be06d88a9c326feefc26e8adde",
 	},
+	vapidKeys: {
+		public: 'BMKp_jwadeX-X4zIX1b4GQxojAtEGpKX9ARTu-8zAHhPKDELypxsveeW6e2xlN5nK8mzvfOpWKAsLzxoaw-pDmE',
+		private: 'YgzGOWoERJDoBDdSlohvorlq-660A9YlvrdHN4YKL88'
+	}
 };
