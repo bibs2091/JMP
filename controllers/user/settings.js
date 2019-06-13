e9cd0a1c6957a678446177e5ab3335eaad2d6aa6
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-06-13 14:28:15 +0100

    adding user dropdown to settings page

--- controllers/user/settings.js
+++ controllers/user/settings.js
@@ -1,6 +1,15 @@
-module.exports = (req, res) => {
+const UsersInfo = require("../../models/UsersInfo");
+
+module.exports = async (req, res) => {
+    //get the current user
+    var currentUser = req.user;
+    var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
+    delete currentUser.password;
+    currentUser.info = userInfo.dataValues;
+
     res.render("user.editprofile2", {
         pageTitle: "Profile settings",
-        pageName: "Profile Settings"
+        pageName: "Profile Settings",
+        currentUser
     });
 }
