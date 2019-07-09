4c376b015d8a99535b8b23ecd5792cb707a69618
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-07-09 04:28:16 +0200

    linking the backend with frontend of user-add-report function

--- controllers/report/add.js
+++ controllers/report/add.js
@@ -4,8 +4,11 @@ db.sync({ forced: true });
 module.exports = async (req, res) => {
 	try{
 			//getting the report data
-	let {userId,indictedType,indictedId} = req.body;
-
+	let userId = req.user.id;
+	let indictedType = req.params.type
+	let indictedId = req.params.id;
+	let type = req.body.type;
+	let reportDetails = req.body.reportDetails
 	const report = await Report.findAll({
 		limit: 1,
 		where:{
@@ -19,19 +22,16 @@ module.exports = async (req, res) => {
 		
 	});
 	// if the report already exists and the admin didn't read it yet
-	if (report){
-		if(!report.read){
+	if(report.length>0)
+		if(report[0].dataValues.read == false){
 			console.log("you already had reported this \n wait till the admin read it");
-			return res.render("404");
+			return res.redirect("/");
 		}
-			
-	}
 	// adding the report
 	await Report.create({userId,type,indictedType,indictedId,reportDetails});
 	console.log("report added");
-	res.render("404");
+	res.redirect("/");
 	}catch (err){
-		console.log("========================================");
 		console.log(err);
 		res.redirect("404");
 	}
--- routes/report.js
+++ routes/report.js
@@ -4,8 +4,9 @@ const router = express.Router();
 //require controllers 
 const reportAddController = require('../controllers/report/add.js');
 //handling requests
-router.post("add/", reportAddController);
-router.get("add/:id",(req, res) => {
+router.post("/add/:id/:type", reportAddController);
+router.get("/add/:id",(req, res) => {
+	console.log("oki=============");
 	res.render("404");
 });
 
