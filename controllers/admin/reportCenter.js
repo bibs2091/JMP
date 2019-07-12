e53f2e1b138cc858f042b3373fa91b298d51bbd1
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-07-12 15:05:12 +0200

    linking front with back of report_center

--- controllers/admin/reportCenter.js
+++ controllers/admin/reportCenter.js
@@ -1,5 +1,16 @@
-module.exports = (req, res) => {
+const Report = require("../../models/Report");
+const User = require("../../models/Users"); 
+module.exports = async (req, res) => {
+	const reports = await Report.findAll({
+		where:{
+			read :false
+		},
+		order: [ [ 'createdAt', 'DESC' ]]
+		
+	});
+	//const user
     res.render("admin.reportCenter", {
+    	reports,
         pageName: "Reports Center",
         pageTitle: "Dashboard - Reports Center"
     });
