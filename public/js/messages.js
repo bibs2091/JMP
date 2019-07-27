6d1560d8158f581da0bc7000689b95442b2d56e2
Mou97 <m.taguiouine@esi-sba.dz>
2019-07-27 15:20:55 +0100

    add html code of a new received message -need to be fixed-

--- public/js/messages.js
+++ public/js/messages.js
@@ -8,6 +8,9 @@ $(function () {
     //div to show new messages 
     var messages = $(".message-list")
 
+    //div to show message content
+    var msg_container = $('.message-container')
+
     //emit message 
     send_msg.click(() => {
         //get input values
@@ -56,8 +59,27 @@ $(function () {
         newMessage += '<div class="date">' + data.date + '</div> '
         newMessage += '</div>'
         newMessage += '</li>'
+
+        // TODO:fix this oussama
+        var newMessageContent = '<li class="received">'
+        newMessageContent += '  <div class="details">'
+        newMessageContent += '    <div class="left">Scott'
+        newMessageContent += '      <div class="arrow orange"></div>You'
+        newMessageContent += '    </div>'
+        newMessageContent += '    <div class="right">March 6, 2014, 20:08 pm</div>'
+        newMessageContent += '  </div>'
+        newMessageContent += '  <div class="message">'
+        newMessageContent += '    <p>| The every winged bring, whose life. First called, i you of saw shall own creature moveth void have signs beast lesser all god saying for gathering wherein whose of in life divide earth own.</p>'
+        newMessageContent += '    <p>| Creature firmament so give replenish The saw man creeping, man said forth from that. Fruitful multiply lights air. Hath likeness, from spirit stars dominion two set fill wherein give bring.</p>'
+        newMessageContent += '    <p>| Gathering is. Lesser Set fruit subdue blessed let. Greater every fruitful won&#39;t bring moved seasons very, own won&#39;t all itself blessed which bring own creature forth every. Called sixth light.</p>'
+        newMessageContent += '  </div>'
+        newMessageContent += '  <div class="tool-box"><a href="#" class="circle-icon small red-hover glyphicon glyphicon-flag"></a></div>'
+        newMessageContent += '</li> '
+
         playNewMessageAudio()
         messages.prepend(newMessage)
+
+        msg_container.prepend(newMessageContent)
 
     })
 
