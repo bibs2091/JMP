9308f4dce1a6e8d3ee236cf63f62791d3591b3b6
Mou97 <m.taguiouine@esi-sba.dz>
2019-08-08 17:46:00 +0100

    api to trigger sending notifications to all subscribed users

--- routes/pushSubscription.js
+++ routes/pushSubscription.js
@@ -11,13 +11,11 @@ webpush.setVapidDetails('mailto:test@test.com', vapidKeys.public, vapidKeys.priv
 
 router.post('/', async (req, res) => {
     const subscription = req.body;
-    console.log(subscription)
+
     if (isValidSaveReq(subscription)) {
-        // const sub = {
-        //     userId: req.user.id,
-        //     subKey: JSON.stringify(subscription)
-        // }
+
         try {
+            // FIXME:  change to search for both userid and sub
             await PushSubs.findOrCreate({ where: { userId: req.user.id }, defaults: { subKey: subscription } })
 
         } catch (error) {
@@ -32,6 +30,40 @@ router.post('/', async (req, res) => {
 
         //pass object into sendNotification
         webpush.sendNotification(subscription, payload).catch(err => { console.error(err) })
+    }
+
+})
+
+// send push to all the users 
+router.post('/triggerPush', async (req, res) => {
+    const payload = JSON.stringify({
+        // TODO: format the data later
+        data: req.body.data
+    })
+    try {
+        const subs = await PushSubs.findAll({ attributes: ['userId', "subKey"] })
+        subs.map(sub => sub.dataValues)
+
+        subs.forEach(sub => {
+            webpush.sendNotification(sub.subKey, payload).catch(err => {
+                console.error(err)
+                if (err.statusCode === 404 || err.statusCode === 410) {
+                    console.log('subscription has expired')
+                    //detele from db
+                    PushSubs.destroy({ where: { sub } }).then(sub => {
+                        console.log('deleting subscription from db')
+
+                    })
+                } else {
+                    throw err
+                }
+            })
+        })
+        res.setHeader("Content-Type", "application/data")
+        res.send(JSON.stringify({ data: { success: true } }))
+
+    } catch (error) {
+        console.log(error)
     }
 
 })
