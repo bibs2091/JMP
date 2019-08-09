cd43b27c43b8011a7b61e819473eaf7ee47b3c17
Mou97 <m.taguiouine@esi-sba.dz>
2019-08-09 20:09:44 +0100

    add route to the api to  search for a user

---
+++ controllers/api/searchUser.js
@@ -0,0 +1,34 @@
+const sequelize = require('sequelize')
+const Op = sequelize.Op
+const Users = require('../../models/UsersInfo')
+
+module.exports = async (req, res) => {
+    const query = req.query.q
+    try {
+        let result = await Users.findAll({
+            where: {
+                username: {
+                    [Op.like]: '%' + query + '%'
+                }
+            }
+        })
+        if (result.length > 0) {
+            let data = result.map(item => {
+                return {
+                    id: item.dataValues.id.toString(),
+                    name: item.dataValues.username
+                }
+            }
+            )
+            console.log(data)
+            res.json(data)
+        } else {
+            res.json({})
+        }
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const router = express.Router();
 //require controllers
 const unapprovedUsersController = require("../controllers/api/unapprovedUsers");
 const allUsersController = require("../controllers/api/allUsers");
-
+const searchUsers = require('../controllers/api/searchUser')
 
 //require middleware
 const isAuthenticated = require("../middleware/isAuthenticated");
@@ -12,6 +12,6 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 //handling requests
 router.get("/users/unapproved", unapprovedUsersController);
 router.get("/users/all", allUsersController);
-
+router.get('/users/search', searchUsers)
 
 module.exports = router;
