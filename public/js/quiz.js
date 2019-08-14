5cca910979ea4e01aac7a426bfd774dad6ff6881
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-08-14 13:58:47 +0100

    refactoring question to cards

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,8 +1,26 @@
 //show next step
-function nextQuest(step) {
+var currentQuestion;
+var numberOfQuestion = $("#quetions-length").html();
+console.log(numberOfQuestion);
+function nextQuest(obj) {
+    console.log(obj);
+    step = obj.getAttribute("data-step");
+    //updating data-step value
+    obj.setAttribute("data-step", parseInt(step) + 1);
+    //hiding all questions
     $(".question").fadeOut();
+    //showing the next question
     $("#question" + step).fadeIn();
+    //updating current question number
     $("#question-number").html(step);
     $(".step").eq(step - 1).addClass("active");
     $(".step").eq(step - 2).addClass("done");
+    //saving the current question
+    currentQuestion = document.getElementById("question" + step);
+    //displaying submit button for the last question
+    if (step == numberOfQuestion) {
+        $("#quiz-next").hide();
+        $("#quiz-submit").show();
+    }
 }
+//validate question radio
