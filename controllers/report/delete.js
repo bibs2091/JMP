37360d0417ff9c7302b74666a8497a6af958668d
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-09-06 04:27:37 +0200

    redo delete report function

--- controllers/report/delete.js
+++ controllers/report/delete.js
@@ -1,6 +1,12 @@
 const Report = require("../../models/Report");
 module.exports = async (req, res) => {
 	try{
+		await Report.destroy({
+	        where : {
+	        	id:req.params.id
+	        }
+	        });
+		res.send({success : true})
 }catch (err){
 		console.log(err);
 		res.redirect("404");
