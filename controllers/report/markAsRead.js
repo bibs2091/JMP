808c43d3ef423dce1858d4612928f35141e44581
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-09-06 04:17:39 +0200

    redo mark report as read function

--- controllers/report/markAsRead.js
+++ controllers/report/markAsRead.js
@@ -1,6 +1,12 @@
 const Report = require("../../models/Report");
 module.exports = async (req, res) => {
 	try{
+		await Report.update(
+			{read:true},
+			{where:
+				{id:req.params.id}
+			});
+		res.send({ success: true });
 }catch (err){
 		console.log(err);
 		res.redirect("404");
