0fb77cb32e4bf89144f8215fc94c6ec7cab355af
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-09-15 14:36:22 +0100

    event page user dropdown

--- controllers/events/eventPage.js
+++ controllers/events/eventPage.js
@@ -1,39 +1,52 @@
 const Event = require('../../models/Event');
 const EventInscriptions = require('../../models/EventInscriptions');
 const Sponsor = require('../../models/Sponsors');
+const UsersInfo = require('../../models/UsersInfo');
+
+
 module.exports = async (req, res) => {
-	try{
+	try {
+		//getting the current user
+		var currentUser = req.user;
+		var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
+		delete currentUser.password;
+		currentUser.info = userInfo.dataValues;
+
 		console.log(req.body);
 		const eventInscriptions = await EventInscriptions.findAll(
-		{
-			where:
 			{
-				userId : req.user.id,
-				eventId : req.params.id
-			}
-		});
+				where:
+				{
+					userId: req.user.id,
+					eventId: req.params.id
+				}
+			});
 		let registred = false;
-		if (eventInscriptions.length ){
+		if (eventInscriptions.length) {
 			registred = true;
 		}
 		const sponsors = await Sponsor.findAll(
-			{where :
+			{
+				where:
 				{
-					eventId : req.params.id
-				}})
+					eventId: req.params.id
+				}
+			})
 		const event = await Event.findByPk(req.params.id);
 		if (!event) {
 			return res.render("404");
 		}
 		event.dataValues.date.getFullYear() + '-' + ('0' + (event.dataValues.date.getMonth() + 1)).slice(-2) + '-' + ('0' + event.dataValues.date.getDate()).slice(-2);
 		res.render('events.event', {
-			pageTitle:"event page",
+			pageTitle: event.name,
 			event,
 			sponsors,
-			registred
+			registred,
+			currentUser,
+			pageName: "Event",
 		});
-	} catch (err){
-		console.log("errors :\n"+err);
+	} catch (err) {
+		console.log("errors :\n" + err);
 		res.redirect('/errors');
 	}
 }
