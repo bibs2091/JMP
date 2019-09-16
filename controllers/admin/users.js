2190380f74210381f0450cf83a33f6b0958eb7ee
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-09-16 17:25:37 +0100

    adding stat cards - managing users

--- controllers/admin/users.js
+++ controllers/admin/users.js
@@ -1,14 +1,25 @@
 const UsersInfo = require("../../models/UsersInfo");
+const Users = require("../../models/Users");
+
 
 module.exports = async (req, res) => {
     var currentUser = req.user;
     var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
     delete currentUser.password;
     currentUser.info = userInfo.dataValues;
+    var unapproved = await Users.findAll({
+        where: {
+            groupId: 3
+        }
+    });
 
     res.render("admin.users", {
         pageName: "Managing Users",
         pageTitle: "Dashboard - Users",
-        currentUser
+        currentUser,
+        unapproved: unapproved.length,
+        students,
+        coaches,
+        admins
     });
 }
