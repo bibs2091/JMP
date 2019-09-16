ae2757ad987d5f83bfa05ecb65ab4ae2b08a3268
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-09-16 17:45:17 +0100

    showing users info on search page

--- controllers/search.js
+++ controllers/search.js
@@ -1,7 +1,15 @@
+const UsersInfo = require("../models/UsersInfo");
+
 module.exports = (req, res) => {
+    var currentUser = req.user;
+    var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
+    delete currentUser.password;
+    currentUser.info = userInfo.dataValues;
+
     console.log(req.query);
     return res.render("search", {
         pageName: "search results",
-        pageTitle: "search for ..."
+        pageTitle: "search for ...",
+        currentUser
     })
 }
