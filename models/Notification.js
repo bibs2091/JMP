c2db543d8184148654e695986b5a1585cf95e89e
Mou97 <m.taguiouine@esi-sba.dz>
2019-09-17 00:37:54 +0100

    create notification model

---
+++ models/Notification.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize");
+const db = require("../config/database");
+
+const Notification = db.define("Notification", {
+    text: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    generated: {
+        type: Sequelize.DATE,
+        defaultValue: Date.now(),
+    },
+    isRead: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+    }
+});
+
+db.sync({ forced: true });
+
+module.exports = Lectures;
