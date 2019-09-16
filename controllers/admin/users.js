6299bd2c5c1ba97bbb1fe4d4fc8fb4b39ca4e711
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-09-16 14:26:58 +0100

    adding user info dropdown - admin/users

--- controllers/admin/users.js
+++ controllers/admin/users.js
@@ -1,6 +1,14 @@
-module.exports = (req, res) => {
+const UsersInfo = require("../../models/UsersInfo");
+
+module.exports = async (req, res) => {
+    var currentUser = req.user;
+    var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
+    delete currentUser.password;
+    currentUser.info = userInfo.dataValues;
+
     res.render("admin.users", {
         pageName: "Managing Users",
-        pageTitle: "Dashboard - Users"
+        pageTitle: "Dashboard - Users",
+        currentUser
     });
 }
