49631c5cb8d3ee9dba0ac9b75809e7c2d59b3750
Mou97 <m.taguiouine@esi-sba.dz>
2019-09-18 00:51:08 +0100

    add function to trigger a push to all subsecribers users

--- controllers/triggerPush.js
+++ controllers/triggerPush.js
@@ -16,8 +16,22 @@ const triggerPush = async (id, payload) => {
     } catch (error) {
         console.log(error)
     }
-
-
+}
+const pushToAllSubsecribers = async (payload) => {
+    try {
+        let req = await axios({
+            method: "POST",
+            url: `http://localhost:3000/subscribe/triggerPush`,
+            data: {
+                payload
+            }
+        })
+        if (req) {
+            return req
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-module.exports = { triggerPush }
+module.exports = { triggerPush, pushToAllSubsecribers }
