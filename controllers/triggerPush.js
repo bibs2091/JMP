271603c7ad846168e0b069a741d28a2d6a291756
Mou97 <m.taguiouine@esi-sba.dz>
2019-09-18 00:22:53 +0100

    add trigger push handler file

---
+++ controllers/triggerPush.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+
+//functions 
+const triggerPush = async (id, payload) => {
+    try {
+        let req = await axios({
+            method: "POST",
+            url: `http://localhost:3000/subscribe/triggerPush/${id}`,
+            data: {
+                payload
+            }
+        })
+        if (req) {
+            return req
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+
+module.exports = { triggerPush }
