8a1a81aa75970f2ef82031c8d07bc18d78772ed5
Mou97 <m.taguiouine@esi-sba.dz>
2019-09-17 18:34:11 +0100

    bug fix : notification stored to db

--- controllers/notificationSys.js
+++ controllers/notificationSys.js
@@ -2,14 +2,13 @@ const Notification = require('../models/Notification')
 
 const sendNotification = async (userId, text) => {
     try {
-        const not = await Notification.create({ userId, text })
+        let not = await Notification.create({ userId: userId, text: text })
         if (not) {
             not = not.dataValues
-            console.log(not)
         }
     } catch (error) {
         console.log(error)
     }
 }
 
-module.exports = sendNotification
+module.exports = { sendNotification }
