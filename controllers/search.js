5ef180545a21770ac3e7fe4841f9d9d6bf3fa111
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-09-17 02:08:26 +0100

    generating search result data

--- controllers/search.js
+++ controllers/search.js
@@ -1,15 +1,58 @@
 const UsersInfo = require("../models/UsersInfo");
+const Events = require("../models/Event");
+const Courses = require("../models/Courses");
+const Sequelize = require("sequelize");
 
 module.exports = async (req, res) => {
-    var currentUser = req.user;
-    var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
-    delete currentUser.password;
-    currentUser.info = userInfo.dataValues;
+    try {
+        var currentUser = req.user;
+        var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
+        delete currentUser.password;
+        currentUser.info = userInfo.dataValues;
 
-    var query = req.query.q;
-    return res.render("search", {
-        pageName: "Search Results",
-        pageTitle: query,
-        currentUser
-    });
+        //getting the search query
+        var query = req.query.q;
+
+        //gettings for users
+        var users = await UsersInfo.findAll({
+            where: {
+                [Sequelize.Op.or]: [
+                    { firstName: { [Sequelize.Op.like]: `%${query}%` } },
+                    { lastName: { [Sequelize.Op.like]: `%${query}%` } },
+                    { username: { [Sequelize.Op.like]: `%${query}%` } }
+                ]
+
+            }
+        });
+        //gettings Events
+        var events = await Events.findAll({
+            where: {
+                [Sequelize.Op.or]: [
+                    { name: { [Sequelize.Op.like]: `%${query}%` } },
+                    { description: { [Sequelize.Op.like]: `%${query}%` } }
+                ]
+            }
+        });
+        //getting courses
+        var courses = await Courses.findAll({
+            where: {
+                [Sequelize.Op.or]: [
+                    { title: { [Sequelize.Op.like]: `%${query}%` } },
+                    { description: { [Sequelize.Op.like]: `%${query}%` } }
+                ]
+            }
+        });
+        return res.render("search", {
+            pageName: "Search Results",
+            pageTitle: "Results for: " + query,
+            currentUser,
+            users,
+            events,
+            courses
+        });
+    }
+    catch (e) {
+        console.log(e.message);
+        res.redirect("/error")
+    }
 }
