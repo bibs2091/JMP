65d4fc4088d348d9e43e6e43dd842637e835364b
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-09-17 03:24:39 +0100

    addin no result found page - search

--- controllers/search.js
+++ controllers/search.js
@@ -12,43 +12,44 @@ module.exports = async (req, res) => {
 
         //getting the search query
         var query = req.query.q;
+        if (query || query.length > 1) {
+            //gettings for users
+            var users = await UsersInfo.findAll({
+                where: {
+                    [Sequelize.Op.or]: [
+                        { firstName: { [Sequelize.Op.like]: `%${query}%` } },
+                        { lastName: { [Sequelize.Op.like]: `%${query}%` } },
+                        { username: { [Sequelize.Op.like]: `%${query}%` } }
+                    ]
 
-        //gettings for users
-        var users = await UsersInfo.findAll({
-            where: {
-                [Sequelize.Op.or]: [
-                    { firstName: { [Sequelize.Op.like]: `%${query}%` } },
-                    { lastName: { [Sequelize.Op.like]: `%${query}%` } },
-                    { username: { [Sequelize.Op.like]: `%${query}%` } }
-                ]
-
+                }
+            });
+            //gettings Events
+            var events = await Events.findAll({
+                where: {
+                    [Sequelize.Op.or]: [
+                        { name: { [Sequelize.Op.like]: `%${query}%` } },
+                        { description: { [Sequelize.Op.like]: `%${query}%` } }
+                    ]
+                }
+            });
+            var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            for (let i = 0; i < events.length; i++) {
+                events[i].month = months[events[i].start_d.substring(5, 7) - 1];
+                let today = new Date();
+                let eveDate = new Date(events[i].start_d.substring(0, 10));
+                events[i].upcoming = eveDate > today;
             }
-        });
-        //gettings Events
-        var events = await Events.findAll({
-            where: {
-                [Sequelize.Op.or]: [
-                    { name: { [Sequelize.Op.like]: `%${query}%` } },
-                    { description: { [Sequelize.Op.like]: `%${query}%` } }
-                ]
-            }
-        });
-        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        for (let i = 0; i < events.length; i++) {
-            events[i].month = months[events[i].start_d.substring(5, 7) - 1];
-            let today = new Date();
-            let eveDate = new Date(events[i].start_d.substring(0, 10));
-            events[i].upcoming = eveDate > today;
+            //getting courses
+            var courses = await Courses.findAll({
+                where: {
+                    [Sequelize.Op.or]: [
+                        { title: { [Sequelize.Op.like]: `%${query}%` } },
+                        { description: { [Sequelize.Op.like]: `%${query}%` } }
+                    ]
+                }
+            });
         }
-        //getting courses
-        var courses = await Courses.findAll({
-            where: {
-                [Sequelize.Op.or]: [
-                    { title: { [Sequelize.Op.like]: `%${query}%` } },
-                    { description: { [Sequelize.Op.like]: `%${query}%` } }
-                ]
-            }
-        });
         return res.render("search", {
             pageName: "Search Results",
             pageTitle: "Results for: " + query,
