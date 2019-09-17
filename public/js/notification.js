9399258b59dbcd62ae1810a7ca88a0b6f42b2b00
Mou97 <m.taguiouine@esi-sba.dz>
2019-09-17 18:35:19 +0100

    bug fix :front-end js for socket

--- public/js/notification.js
+++ public/js/notification.js
@@ -1,3 +1,10 @@
-$(function () {
-    console.log('notification script is ready')
+
+// console.log('notification script is ready')
+var notifTest = $('#notifTest')
+console.log('loaded in coach')
+socket.on('notification', (data) => {
+
+    console.log(data)
+    notifTest.append('<h1>' + data + '</h1>')
+
 })
