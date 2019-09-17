a101be4503806a280448f339acc5bb16c795f33f
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-09-18 00:57:48 +0100

    adding admin courses page

--- controllers/admin/courses.js
+++ controllers/admin/courses.js
@@ -1,4 +1,5 @@
 const UsersInfo = require("../../models/UsersInfo");
+const Courses = require("../../models/Courses");
 
 module.exports = async (req, res) => {
     var currentUser = req.user;
@@ -6,9 +7,15 @@ module.exports = async (req, res) => {
     delete currentUser.password;
     currentUser.info = userInfo.dataValues;
 
+    var courses = await Courses.findAll();
+    for (let i = 0; i < courses.length; i++) {
+        var author = await UsersInfo.findOne({ where: { userId: courses[i].author } });
+        courses[i].author = author.firstName + " " + author.lastName;
+    }
     res.render("admin.courses", {
         pageName: "Courses",
         pageTitle: "Dashbaord - Courses",
-        currentUser
+        currentUser,
+        courses
     });
 }
