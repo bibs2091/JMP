ff3904c6805ded23ff474a398787a6d6b512e278
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-09-17 10:59:40 +0100

    fixing admin dropdown bug

--- controllers/admin/settings.js
+++ controllers/admin/settings.js
@@ -1,6 +1,14 @@
+const UsersInfo = require("../../models/UsersInfo");
+
 module.exports = async (req, res) => {
+    var currentUser = req.user;
+    var userInfo = await UsersInfo.findOne({ where: { userId: req.user.id } });
+    delete currentUser.password;
+    currentUser.info = userInfo.dataValues;
+
     res.render("admin.settings", {
         pageName: "Settings",
-        pageTitle: "Settings - Dashboard"
+        pageTitle: "Settings - Dashboard",
+        currentUser
     });
 }
