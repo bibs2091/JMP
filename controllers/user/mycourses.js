177d4955b84579f5bac0e42b79d45ceb297953c0
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-09-19 07:04:21 +0100

    adding author image

--- controllers/user/mycourses.js
+++ controllers/user/mycourses.js
@@ -29,6 +29,7 @@ module.exports = async (req, res) => {
         var duration = "";
         var author = await UsersInfo.findOne({ where: { userId: courses[i].author } });
         courses[i].author = author.firstName + " " + author.lastName;
+        courses[i].authorImage = author.avatar;
         courses[i].duration = Math.floor(courses[i].duration / 60);
         if (courses[i].duration > 60) {
             duration += Math.floor(courses[i].duration / 60) + " h ";
